e88b77f9079fccaa464290f13b34d614d4f8740b
ObedOlavarria <129216824+ObedOlavarria@users.noreply.github.com>
2024-05-13 09:47:09 -0600

    Update catalogo.php
    una prueba OO

---
+++ catalogo.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="images/Logo_AutoAI.png" type="image/png">
+    <title>Coches</title>
+</head>
+
+<body>
+    <div class="spacer">
+        <span class="call-us">Llámenos: <a href="tel:+529999024520">+52 (999)9 024 520</a></span>
+
+        <span class="support">Soporte: <a href="tel:+529999024520">+52 (999)9 024 520</a></span>
+
+        <a class="access" href="login.html">Acceso</a>
+
+        <a class="register" href="register.html">Registro</a>
+
+    </div>
+
+    <div class="menu">
+        <img src="images/logo.png" alt="">
+        <a class="home" href="index.html">Inicio</a>
+        <a href="about.html">Sobre Nosotros</a>
+        <a class="cars-menu" href="cars.html">Coches</a>
+        <a href="location.html">Ubicación</a>
+        <a href="contact.html">Contáctenos</a>
+    </div>
+    <div class="cars">
+        <div class="sports">
+            <img src="images/Skyline_GTR_V-Spec_II_Nur_(BNR34).png" alt="">
+            <h3>Coches deportivos</h3>
+            <br>
+            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
+                <option value="">Coches</option>
+                <option value="">Toyota Supra</option>
+                <option value="">Ford Mustang</option>
+                <option value="">Chevrolet Corvette</option>
+            </select>
+        </div>
+        <div class="classics">
+            <img src="images/Honda_Civic_Type_R_(EK9).png" alt="">
+            <br><br>
+            <h3>Coches clásicos</h3>
+            <br>
+            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
+                <option value="">Coches</option>
+                <option value="">Plymouth Barracuda – 1969</option>
+                <option value="">Mustang Boss 429 – 1969</option>
+                <option value="">Chevelle SS – 1969</option>
+            </select>
+        </div>
+        <div class="new">
+            <img src="images/aston_martin_dbx707.png" alt="">
+            <h3>Coches nuevos</h3>
+            <br>
+            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
+                <option value="">Coches</option>
+                <option value="">Honda CR-V</option>
+                <option value="">Toyota Corolla</option>
+                <option value="">Hyundai Elantra</option>
+            </select>
+        </div>
+    </div>
+    <br><br>
+    <div class="cars">
+        <div class="used">
+            <img src="images/RX-7_Infiniti_III_(FC3S).png" alt="">
+            <br><br>
+            <h3>Coches usados</h3>
+            <br>
+            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
+                <option value="">Coches</option>
+                <option value="">Fiat 500 Abarth</option>
+                <option value="">Kia Forte</option>
+                <option value="">Mini Cooper S Hot Chili</option>
+            </select>
+        </div>
+        <div class="agency">
+            <img src="images/RX-8_Type_S_(SE3P).png" alt="">
+            <br><br>
+            <h3>Coches de agencia</h3>
+            <br>
+            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
+                <option value="">Coches</option>
+                <option value="">Acura Integra</option>
+                <option value="">Alfa Romeo Stelvio</option>
+                <option value="">Aston Martin DB
