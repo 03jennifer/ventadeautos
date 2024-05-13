1e1da76978d29ddc68dcdda64ba4c8aaf78bc6f9
ObedOlavarria <129216824+ObedOlavarria@users.noreply.github.com>
2024-05-13 10:01:16 -0600

    Update catalogo.php
    agregar ccs

--- catalogo.php
+++ catalogo.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="estilo_catalogo.css"> <!-- Aquí se agrega el nuevo archivo CSS -->
     <link rel="icon" href="images/Logo_AutoAI.png" type="image/png">
     <title>Coches</title>
 </head>
@@ -27,65 +28,4 @@
         <a href="about.html">Sobre Nosotros</a>
         <a class="cars-menu" href="cars.html">Coches</a>
         <a href="location.html">Ubicación</a>
-        <a href="contact.html">Contáctenos</a>
-    </div>
-    <div class="cars">
-        <div class="sports">
-            <img src="images/Skyline_GTR_V-Spec_II_Nur_(BNR34).png" alt="">
-            <h3>Coches deportivos</h3>
-            <br>
-            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
-                <option value="">Coches</option>
-                <option value="">Toyota Supra</option>
-                <option value="">Ford Mustang</option>
-                <option value="">Chevrolet Corvette</option>
-            </select>
-        </div>
-        <div class="classics">
-            <img src="images/Honda_Civic_Type_R_(EK9).png" alt="">
-            <br><br>
-            <h3>Coches clásicos</h3>
-            <br>
-            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
-                <option value="">Coches</option>
-                <option value="">Plymouth Barracuda – 1969</option>
-                <option value="">Mustang Boss 429 – 1969</option>
-                <option value="">Chevelle SS – 1969</option>
-            </select>
-        </div>
-        <div class="new">
-            <img src="images/aston_martin_dbx707.png" alt="">
-            <h3>Coches nuevos</h3>
-            <br>
-            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
-                <option value="">Coches</option>
-                <option value="">Honda CR-V</option>
-                <option value="">Toyota Corolla</option>
-                <option value="">Hyundai Elantra</option>
-            </select>
-        </div>
-    </div>
-    <br><br>
-    <div class="cars">
-        <div class="used">
-            <img src="images/RX-7_Infiniti_III_(FC3S).png" alt="">
-            <br><br>
-            <h3>Coches usados</h3>
-            <br>
-            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
-                <option value="">Coches</option>
-                <option value="">Fiat 500 Abarth</option>
-                <option value="">Kia Forte</option>
-                <option value="">Mini Cooper S Hot Chili</option>
-            </select>
-        </div>
-        <div class="agency">
-            <img src="images/RX-8_Type_S_(SE3P).png" alt="">
-            <br><br>
-            <h3>Coches de agencia</h3>
-            <br>
-            <select class="vehicles" name="" id="" style="background-color: #CB4D1A;">
-                <option value="">Coches</option>
-                <option value="">Acura Integra</option>
-                <option value="">Alfa Romeo Stelvio</option>
-                <option value="">Aston Martin DB
+        <a href="contact.html
