0c934fb1892bcfd4208209e6d3b646510cb8fb27
03jennifer <147662430+03jennifer@users.noreply.github.com>
2024-05-20 00:49:17 -0600

    Update vista_usuario.php

--- vista_usuario.php
+++ vista_usuario.php
@@ -1,20 +1,7 @@
 <?php
 session_start();
 
-if (isset($_SESSION['usuario'])) {
-    // Obtener el género del usuario
-    $genero = $_SESSION['genero'];
-
-    // Mostrar un mensaje de bienvenida personalizado
-    if ($genero == 'Hombre') {
-        echo "<p>Bienvenido, caballero. Disfruta de tu experiencia en nuestro sitio.</p>";
-    } elseif ($genero == 'Mujer') {
-        echo "<p>Bienvenida, dama. Esperamos que te sientas cómoda en nuestro sitio.</p>";
-    } else {
-        echo "<p>Bienvenido/a. Disfruta de tu experiencia...</p>";
-    }
-} else {
-    // Si el usuario no ha iniciado sesión, redirigirlo al formulario de inicio de sesión
+if (!isset($_SESSION['usuario'])) {
     header("Location: login.php");
     exit();
 }
@@ -25,13 +12,54 @@ if (isset($_SESSION['usuario'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="estilo_vistas.css">
     <title>Bienvenido, <?php echo $_SESSION['usuario']; ?> Usuario</title>
+    <link rel="stylesheet" href="estilo_vistas.css">
+
 </head>
 <body>
-    <h1><?php echo $_SESSION['usuario']; ?></h1>
-    <p>Esta es tu página de inicio después de iniciar sesión.</p>
-    <p><a href="cerrar_sesion.php">Cerrar Sesión</a></p>
+    <div class="toolbar">
+        <div class="toolbar-container">
+            <div class="toolbar-section">
+                <a href="vista_usuario.php" id="casa">
+                    <img width="50px" height="40px" 
+                    src="img/minimal-house-icon-website-symbol-site-sign-ui-home-icon-home-creative-icon-minimalist-vector.jpg" alt="Inicio">
+                    <br>Inicio
+                </a>
+            </div>
+            <div class="toolbar-section">
+                <a href="perfil.php">Perfil</a>
+            </div>
+            <div class="toolbar-section">
+                <a href="usuario_compras.php">Mis Compras</a>
+            </div>
+            <div class="toolbar-section">
+                <a href="usuario_catalogo.php">Catálogo</a>
+            </div>
+            <div class="toolbar-section">
+                <a href="cerrar_sesion.php">Cerrar Sesión</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="bienvenida">
+            <?php
+            // Obtener el género del usuario
+            $genero = $_SESSION['genero'];
+
+            // Mostrar un mensaje de bienvenida personalizado
+            if ($genero == 'Hombre') {
+                echo "<p>Bienvenido, caballero.</p>";
+            } elseif ($genero == 'Mujer') {
+                echo "<p>Bienvenida, dama.</p>";
+            } else {
+                echo "<p>Bienvenido/a. Disfruta de tu experiencia...</p>";
+            }
+            ?>
+        </div>
+
+        <h1><?php echo $_SESSION['usuario']; ?></h1>
+        <p>Esta es tu página de inicio después de iniciar sesión.</p>
+    </div>
 </body>
 </html>
